0fcc6f5ec4411f52242bdd61d931bae1ae8fe8c7
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2023-04-17 16:39:56 +0500

    creating button component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,33 +1,47 @@
-'use client';
+import { SafeUser } from "@/app/types";
 
-import React from 'react';
+import Categories from "./Categories";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
-import Container from '../Container';
-import Logo from './Logo';
-import Search from './Search';
-import UserMenu from './UserMenu';
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
 
-const Navbar = () => {
-  return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b-[1px]'></div>
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
       <Container>
-        <div
-          className='
+        <div 
+          className="
             flex 
             flex-row 
             items-center 
             justify-between
             gap-3
             md:gap-0
-          '>
+          "
+        >
           <Logo />
           <Search />
-          <UserMenu />
+          <UserMenu currentUser={currentUser} />
         </div>
       </Container>
     </div>
+    <Categories />
+  </div>
   );
-};
+}
+
 
 export default Navbar;
